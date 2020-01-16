03115c8835991536af5294a54d46bdd0484eda68
lihuihui123456 <18336070328@163.com>
2020-01-16 15:42:41 +0800

    添加菜单
    添加菜单

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,7 +16,6 @@ Page({
     })
   },
   onLoad: function () {
-    console.log(app.globalData.userInfo);
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
